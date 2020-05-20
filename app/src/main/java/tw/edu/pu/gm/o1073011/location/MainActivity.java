c957fbf5b00033d9bb0b8cefed76b49d2944cd99
楊忠誠 <o1073o11@gm.pu.edu.tw>
2020-05-20 10:40:44 +0800

    step 3

--- app/src/main/java/tw/edu/pu/gm/o1073011/location/MainActivity.java
+++ app/src/main/java/tw/edu/pu/gm/o1073011/location/MainActivity.java
@@ -1,11 +1,14 @@
 package tw.edu.pu.gm.o1073011.location;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     //可自行設定請求代碼，區別用
@@ -13,6 +16,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     LocationManager locationManager;
 
+    private void CheckPermission(){
+        //如果使用者尚未允許權限
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "本App需允許位置授權，才能定位",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
